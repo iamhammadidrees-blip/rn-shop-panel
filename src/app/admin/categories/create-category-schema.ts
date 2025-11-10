8b301dd8e8789725cd299f63a55eb1bec82accf5
M.Hammad Idrees <iamhammadidrees@gmail.com>
2025-11-10 20:09:30 +0500

    refactor: updated the Route, schemas,  Database type
    -[create-category-schema.ts] -  Validation schemas for form data inputs
    -[types.ts]  - Database type definitions
    -[page.tsx]  - Router to AUTH page Directly in WEB

--- src/app/admin/categories/create-category-schema.ts
+++ src/app/admin/categories/create-category-schema.ts
@@ -6,3 +6,19 @@ export const createCategorySchema = z.object({
 });
 
 export type CreateCategorySchema = z.infer<typeof createCategorySchema>;
+
+export const createCategorySchemaServer = z.object({
+    imageUrl: z.string().min(1, { message: 'Image is required' }),
+    name: z.string().min(2, { message: 'Name must be at least 2 characters' })
+});
+
+export type CreateCategorySchemaServer = z.infer<typeof createCategorySchemaServer>;
+
+export const updateCategorySchema = z.object({
+    imageUrl: z.string().min(1, { message: 'Image is required' }),
+    name: z.string().min(2, { message: 'Name must be at least 2 characters' }),
+    slug: z.string().min(1, { message: 'Slug is required' }),
+    intent: z.enum(['update']).optional()
+});
+
+export type UpdateCategorySchema = z.infer<typeof updateCategorySchema>;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+import Auth from "./auth/page";
+
 export default function Home() {
-  return <>HOME Page:</>
+//  return <>HOME Page:</>
+return <Auth/>;
 }
